ca95794baaa99d3fe143b2f89064f0ecf49f0399
Renato <rnt.graca01@gmail.com>
2023-09-09 11:58:19 -0300

    User entity and resource

---
+++ src/main/java/com/renatosilv/webServicesSpringBOOT/resources/UserRersource.java
@@ -0,0 +1,23 @@
+package com.renatosilv.webServicesSpringBOOT.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.renatosilv.webServicesSpringBOOT.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserRersource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria", "maria@gmail.com","123456789","12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	
+	
+}
