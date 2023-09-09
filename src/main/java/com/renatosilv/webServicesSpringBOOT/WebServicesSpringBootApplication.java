5e9eb9d7887571eced8e126efd6a5f8aa574dafd
Renato <rnt.graca01@gmail.com>
2023-09-09 11:25:59 -0300

    Project created

---
+++ src/main/java/com/renatosilv/webServicesSpringBOOT/WebServicesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.renatosilv.webServicesSpringBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renatosilv/webServicesSpringBOOT/WebServicesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatosilv.webServicesSpringBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
